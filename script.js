3eb5dd0b2ff6ff8678f7e2af2aa538ff0ef6b2b3
Hanan-Mir <Hanan15ite13@gmail.com>
2024-11-12 22:26:47 +0530

    completed the loan login summary filter features

--- script.js
+++ script.js
@@ -63,14 +63,300 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// LECTURES
+containerMovements.innerHTML='';
+//--------------FUNCTION FOR DISPLAYING THE MOVEMENTS FOR OUR APPLICATION----------------------------------
+const displayMovements=function(movement){
+  containerMovements.innerHTML='';
+movement.forEach(function(value,index,mov){
 
+  let type=value>0?"deposit":"withdrawal";
+  const transactionElement=`<div class="movements__row">
+  <div class="movements__type movements__type--${type}">${index+1} ${type}</div>
+  <div class="movements__date">3 days ago</div>
+  <div class="movements__value">${value}</div>
+</div>
+  `
+  containerMovements.insertAdjacentHTML('afterbegin',transactionElement);
+})
+
+}
+//displayMovements(account1.movements);
+//------------------------------------COMPUTE USER NAMES----------------------
+let calcUserName=function(acc){
+  acc.forEach(function(value){
+    value.UserName=value.owner.toLowerCase().split(' ').map(el=>el[0]).join('');
+  })
+}
+calcUserName(accounts);
+//------------------------------------TOTAL AMOUNT----------------------------
+// let totalMovementSum=function(acc){
+// acc.accountBalance=acc.forEach(function(value){
+//   let sum=0;
+//   value.movements.reduce(function(accum,el,index,arr){
+//   sum=accum+el;
+//   return sum;
+// },0)
+// labelBalance.textContent=`${value.accountBalance}EUR`;
+// })
+
+// }
+let totalMovementSum=function(acc){
+ acc.balance=acc.movements.reduce((accu,value,index,arr)=>{ return accu+value;
+},0)
+labelBalance.textContent=`${acc.balance} EUR`;
+}
+
+//totalMovementSum(account1);
+//-----------------calculate summary----------------------------
+const calcSummary=function(acc){
+  const deposits=acc.movements.filter(amount=>amount>0).reduce((acc,curVal)=>acc+curVal,0);
+  labelSumIn.textContent=`${deposits}`;
+  const withdrawls=acc.movements.filter(amount=>amount<0).reduce((acc,currVal)=>acc+currVal,0);
+  labelSumOut.textContent=`${withdrawls}`;
+  const intrests=acc.movements.filter(amount=>amount>0).map(val=>(val*acc.interestRate)/100).filter(el=>el>=1).reduce((acc,curVal)=>acc+curVal,0);
+  labelSumInterest.textContent=`${intrests}`;
+}
+//calcSummary(account1.movements);
+//--------------------------update UI---------------------------
+let updateUI=function(acc){
+calcSummary(acc);
+totalMovementSum(acc);
+displayMovements(acc.movements);
+}
+
+
+
+
+
+//-------------------------------------------------------LOGIN FUNCTION----------------------------------
+let currentUser;
+btnLogin.addEventListener('click',function(e){
+  e.preventDefault();
+currentUser=accounts.find((acc)=>acc.UserName===inputLoginUsername.value)
+if(currentUser?.pin===Number(inputLoginPin.value)){
+  //change opacity
+  containerApp.style.opacity=1;
+  //display user Name
+  labelWelcome.textContent=`Welcome back ,${currentUser.owner.split(' ')[0]}`
+  // remove the login and pin 
+  inputLoginPin.value=inputLoginUsername.value='';
+  inputLoginPin.blur();
+//display movements;
+displayMovements(currentUser.movements);
+//display summary
+calcSummary(currentUser);
+//display total movements sum
+totalMovementSum(currentUser);
+//get array dynamically 
+let myArray=Array.from(document.querySelectorAll('.movements__value'),(el)=>Number(el.textContent));
+console.log(myArray)
+}
+})
+//----------------------------transfer function-------------------------
+btnTransfer.addEventListener('click',function(e){
+  e.preventDefault();
+  let amount=Number(inputTransferAmount.value);
+  let transferTo=accounts.find(acc=>acc.UserName===inputTransferTo.value);
+if(transferTo && currentUser.balance>amount &&transferTo!==currentUser.UserName &&amount >0){
+  transferTo.movements.push(amount);
+  currentUser.movements.push(amount);
+  updateUI(currentUser);
+  
+}
+inputTransferAmount.value=inputTransferTo.value=' ';
+})
+//--------------------------------delete account---------------
+btnClose.addEventListener('click',function(e){
+e.preventDefault();
+const userPin=Number(inputClosePin.value);
+const userCloseName=inputCloseUsername.value;
+if(userCloseName===currentUser.UserName && userPin===currentUser.pin){
+  let getIndex=accounts.findIndex((acc)=>acc.UserName===userCloseName)
+  accounts.splice(getIndex,1);
+  containerApp.style.opacity=0;
+  
+}
+inputClosePin.value=inputCloseUsername.value='';
+})
+//-----------------------------------loan request------------------------------
+btnLoan.addEventListener('click',function(e){
+  e.preventDefault();
+  console.log('loan passed');
+  const loanAmount=Number(inputLoanAmount.value);
+  let checkEligbility=currentUser.movements.some(el=>el>=loanAmount*0.1);
+  if(checkEligbility){
+    currentUser.movements.push(loanAmount);
+    updateUI(currentUser);
+  }
+})
+//--------------------------------sort movements---------------------------------
+let sorted=false;
+btnSort.addEventListener('click',function(e){
+  e.preventDefault();
+  //console.log('sort clicked');
+  if(sorted===false){
+    currentUser.movements.sort((a,b)=>a-b);
+displayMovements(currentUser.movements);
+sorted=true;
+  }
+  else{
+    sorted=false;
+    currentUser.movements.sort((a,b)=>b-a);
+    displayMovements(currentUser.movements); 
+  }
+})
+// LECTURES
+/////////////////////////////////////////////////
+//------------------------------------ARRAY METHODS-----------------------------------
+const arr1=['a','b','c','d','e'];
+//-----------------slice method---------------------------------
+console.log(arr1.slice(2));
+console.log(arr1.slice(2,4));
+console.log(arr1.slice(-1));
+console.log(arr1.slice(2,-1));
+console.log(arr1.slice(-3));
+//------------------splice method------------------------------
+//console.log(arr1.splice(-1));
+//console.log(arr1.splice(1,3));
+console.log(arr1.splice(-3,2));
+console.log(arr1.splice(2,0,'hanan','rashid','yawer'));
+console.log(arr1);
+console.log(arr1.reverse());
+console.log(arr1);
+const arr2=['yasir','shuail','majid',"Rohan"];
+//------------concat method-----------------
+let newArray=arr1.concat(arr2);
+console.log(newArray);
+//--------------join method------------------
+let newArr2=arr2.join('+');
+console.log(newArr2);
+//------------at method--------------------
+console.log(newArray.at(2));
+//-------------------------forEach method------------------------
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(`--------------FOR LOOP-------------------`);
+for (let [index,value] of movements.entries()){
+  if(value>0){
+    console.log(`Transaction ${index+1}:Amount deposited ${value}`);
+  }
+  else{
+    console.log(`Transaction ${index+1}:Amount withdrew ${Math.abs(value)}`);
+  }
+}
+console.log(`-----------------FOR EACH LOOP---------------------`);
+movements.forEach(function(value,ind,mov){
+  if(value>0){
+    console.log(`Transaction ${ind+1}:Amount deposited ${value}`);
+  }
+  else{
+    console.log(`Transaction ${ind+1}:Amount withdrew ${Math.abs(value)}`);
+  }
+})
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
+//-----------forEach for maps----------------
+currencies.forEach(function(value,key,map){
+  console.log(`${key}:${value}`);
+})
+//-------forEach for sets------------------
+let mySet =new Set(['a','b','c','a','a','a']);
+console.log(mySet);
+mySet.forEach(function(value,key,mySet){
+  console.log(`${key}:${value}`)
+})
+//-------------------------------------CODING CHALLENGE #1-------------------------------
+let checkDogs=function(arr1,arr2){
+  let newDogsArray=arr1.slice(1);
+  console.log(newDogsArray);
+newDogsArray=newDogsArray.slice(0,2);
+console.log(newDogsArray);
+let allDogsArray=newDogsArray.concat(arr2);
+allDogsArray.forEach(function(value,index,arr){
+  if(value>=3){
+    console.log(`Dog number ${index+1} is an adult, and is ${value} years old`)
+  }
+  else{
+    console.log(`Dog number ${index +1} is still a puppy`)
+  }
+})
+}
+let dogsJulia=[3,5,2,12,7];
+let dogsKate=[4,1,15,8,3];
+checkDogs(dogsJulia,dogsKate);
+//----------------------------------MAP METHOD----------------------
+const transactions=[100,2000,600,900,-1000,900,-8000,-5560];
+const euroToRs=100;
+const movementsRs=transactions.map((value)=>{
+  return value*euroToRs;
+})
+console.log(movementsRs);
+//-----------by for loop
+console.log(`--------using for loop------------`);
+const movementRsFor=[];
+for(let [i,v] of transactions.entries()){
+  movementRsFor.push(v*euroToRs);
+}
+console.log(movementRsFor);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+let movementsDescription=transactions.map((value,index)=>{
+  return `Transaction ${index +1}:You ${value>0?"DEPOSITED":"WITHDREW"} ${value}`;
+})
+console.log(movementsDescription);
+//-------------------------------------coding challenge #2--------------------------
+let calcAverageHumanAge=function(dogAges){
+  console.log(dogAges);
 
-/////////////////////////////////////////////////
+  // dogAges.filter(function(value){
+  //   if(value<=2){
+  //    humanAge.push(value*2);
+  //   }
+  //   if(value>2){
+  //     humanAge.push(16+value*4);
+  //   }
+   let humanAge=dogAges.map(value=>(value<=2)? value*2:16+value*4);
+   console.log(humanAge);
+   humanAge=humanAge.filter(value=>value>=18);
+   console.log(humanAge);
+   let average=humanAge.reduce((accum,value,index,arr)=>{
+      return accum+value;
+   },0)/humanAge.length;
+   console.log(average);
+  }
+let calcAverageHumanAge2=function(dogagesArray){
+  let length;
+  let humanAge=dogagesArray.map(value=>(value<=2)? value*2:16+value*4).filter(ages=>{
+     return ages>=18}).reduce((acc,val,index,arr)=>{
+
+    return acc+val/arr.length},0);
+    console.log(humanAge);
+}
+  
+
+let dogsage=[5,2,4,1,15,8,3]
+calcAverageHumanAge(dogsage);
+calcAverageHumanAge2(dogsage);
+ //----------------------------------------------Find Method---------------------------------
+ let findAccount=function(accou){
+ accou.find(function(acc){
+if(acc.owner==='Jessica Davis'){
+console.log(acc);
+}
+ })
+ }
+findAccount(accounts);
+let findAccount2=function(acc){
+  for(let val of acc){
+    if(val.owner==='Sarah Smith'){
+      console.log(val);
+  }
+}
+}
+findAccount2(accounts);
+//----------------------------FLAT AND FLATMAP------------------------------------------
+let newMovement=[1000,5000,-799,5000];
+let isDeposits=newMovement.some(el=>el>0);
+console.log(isDeposits);
+//--------------------------creating an Array dynamically-------------------------
